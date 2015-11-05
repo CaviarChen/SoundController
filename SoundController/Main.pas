3dbdfb6eba33044d3e5a7a85402bf342057752fc
CaviarChen <yzj@vlabpro.com>
2015-11-05 11:16:53 +1100

    Finish

--- SoundController/Main.pas
+++ SoundController/Main.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Media;
 
 type
   TForm1 = class(TForm)
@@ -18,9 +18,13 @@ type
     Button_play: TButton;
     Text4: TText;
     Text5: TText;
+    Text6: TText;
+    Button_back: TButton;
     procedure FormShow(Sender: TObject);
     procedure Button_pClick(Sender: TObject);
     procedure Button_nClick(Sender: TObject);
+    procedure Button_playClick(Sender: TObject);
+    procedure Button_backClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -39,6 +43,7 @@ uses IOUtils,XSuperObject;
 
 var config:ISuperObject;
     currentid:integer;
+    MediaPlayer:array[1..4] of TMediaPlayer;
 
 function LoadJson(f:string):Isuperobject;
 var
@@ -71,6 +76,15 @@ end;
 
 procedure TForm1.setGUI();
 begin
+  if currentid=config.A['Actions'].Length then
+    begin
+      Panel1.Visible := False;
+      Button_p.Enabled := False;
+      Button_n.Enabled := False;
+      Exit;
+    end;
+
+
   Button_p.Enabled := not(currentid=0);
   Button_n.Enabled := not(currentid=config.A['Actions'].Length-1);
   Text1.Text := Format('%d out of %d',[currentid+1,config.A['Actions'].Length]);
@@ -88,6 +102,13 @@ begin
 end;
 
 
+procedure TForm1.Button_backClick(Sender: TObject);
+begin
+  currentid := currentid-1;
+  setGUI;
+  Panel1.Visible := True;
+end;
+
 procedure TForm1.Button_nClick(Sender: TObject);
 begin
   currentid := currentid+1;
@@ -100,8 +121,35 @@ begin
   setGUI;
 end;
 
+procedure TForm1.Button_playClick(Sender: TObject);
+var player:integer;
+begin
+  player := config.A['Actions'].O[currentid].I['player'];
+
+  if config.A['Actions'].O[currentid].S['type']='stop' then
+    begin
+      MediaPlayer[player].Stop;
+    end;
+
+  if config.A['Actions'].O[currentid].S['type']='changevolume' then
+    begin
+      MediaPlayer[player].Volume := config.A['Actions'].O[currentid].F['volume'];
+    end;
+
+  if config.A['Actions'].O[currentid].S['type']='play' then
+    begin
+      MediaPlayer[player].Stop;
+      MediaPlayer[player].Volume := config.A['Actions'].O[currentid].F['volume'];
+      MediaPlayer[player].FileName := config.A['Actions'].O[currentid].S['file'];
+      MediaPlayer[player].Play;
+    end;
+
+  currentid := currentid+1;
+  setGUI;
+end;
+
 procedure TForm1.FormShow(Sender: TObject);
-var r:integer;
+var r,i:integer;
 begin
   r := LoadConfig;
   if r<>0 then
@@ -110,6 +158,9 @@ begin
       if r=-2 then ShowMessage('There is no action in the configuration.');
       Self.Close;
     end;
+
+  for i := 1 to 4 do
+    MediaPlayer[i] := TMediaPlayer.Create(nil);
 
 end;
 
